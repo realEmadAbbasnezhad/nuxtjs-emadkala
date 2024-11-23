373c2d5df9348ed4e98bbbb943b9e0681e780b63
Emad Abbasnezhad <real_emad_abbasnezhad@proton.me>
2024-11-23 14:06:34 +0330

    add tailwind package, config quasar theme, config tailwind theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,26 @@
+import colors from "tailwindcss/colors";
+
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: {enabled: true},
-    modules: ['nuxt-quasar-ui'],
-    quasar: {}
+    modules: ['nuxt-quasar-ui', '@nuxtjs/tailwindcss'],
+    tailwindcss: {
+        config: {
+            prefix: "tw-",
+            theme: {
+                colors: {
+                    primary: colors.sky,
+                    background: colors.white
+                }
+            }
+        }
+    },
+    quasar: {
+        config: {
+            brand: {
+                primary: colors.sky[500],
+                background: colors.white
+            },
+        },
+    }
 })
